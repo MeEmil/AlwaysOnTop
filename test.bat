2b254b05d5e27894f467accfacd7ed7b3bcc6f58
MeEmil <43099378+MeEmil@users.noreply.github.com>
2018-09-08 23:19:08 +0200

    Uses username relative to the user when launching

--- test.bat
+++ test.bat
@@ -1,2 +1,2 @@
-start "C:\Program Files\AutoHotkey\AutoHotkey.exe" "C:\Users\Emil\Desktop\ProgramsIUse\Alwaysontop.ahk"
+start "C:\Program Files\AutoHotkey\AutoHotkey.exe" "C:\Users\%username%\Desktop\ProgramsIUse\Alwaysontop.ahk"
 echo "Done"
